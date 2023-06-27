0ab69f2a8f8226db92f0a6d96d0ba26dd8d06f58
theunhackable <psrsai001@gmail.com>
2023-06-27 11:05:48 +0530

    renamed:    app/logo.ico -> app/favicon.ico

--- app/layout.jsx
+++ app/layout.jsx
@@ -10,10 +10,6 @@ export const metadata =  {
 const RootLayout = ({children}) => {
   return (
     <html lang='en'>
-      <head>
-      <link rel="shortcut icon" href="./logo.ico" sizes="32x32"/>
-
-      </head>
         <body>
           <Provider>
             <div className="main">
